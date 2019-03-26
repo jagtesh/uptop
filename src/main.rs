01a72ca6f81b7036d827c3141e0c02928ec03d67
Jagtesh Chadha <jagtesh@gmail.com>
2019-03-26 14:54:57 +0000

    feat: added skeleton with basic pid info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use sysinfo::{ProcessExt, SystemExt};
+
 fn main() {
-    println!("Hello, world!");
+    let mut system = sysinfo::System::new();
+
+    system.refresh_all();
+
+    for (pid, proc_) in system.get_process_list() {
+        println!("{:6}\t{:<30}\t{:<10}", pid, proc_.name(), proc_.status());
+    }
 }
