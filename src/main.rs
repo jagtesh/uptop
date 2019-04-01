58dba5d3ce28a32dc1ea0e82521d78629c75329b
Jagtesh Chadha <jagtesh@gmail.com>
2019-04-01 05:27:49 +0000

    refactor: new function for formatting output

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use sysinfo::{ProcessExt, SystemExt};
 
+fn format_process_list(pid: &i32, proc: &sysinfo::Process) -> String {
+    return format!("{:6}\t{:<30}\t{:<10}", pid, proc.name(), proc.status());
+}
+
 fn main() {
     let mut system = sysinfo::System::new();
 
     system.refresh_all();
 
-    for (pid, proc_) in system.get_process_list() {
-        println!("{:6}\t{:<30}\t{:<10}", pid, proc_.name(), proc_.status());
+    for (pid, proc) in system.get_process_list() {
+        println!("{}", format_process_list(pid, proc));
     }
 }
